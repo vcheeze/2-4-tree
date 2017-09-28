15814dba1c5d0b9e48e23abd60f850967c70f7c9
Peter Chen <manchesterutd7@gmail.com>
2017-09-28 15:24:30 +0400

    basic implementation of linked list done

--- LinkedList.h
+++ LinkedList.h
@@ -8,6 +8,7 @@
 
 #include <string>
 #include <utility>
+#include <iostream>
 
 using namespace std;
 
@@ -15,7 +16,7 @@ struct node {
     string courseID;
     string courseName;
     string grade;
-    node* next;
+    node* next = nullptr;
 };
 
 
@@ -25,7 +26,14 @@ public:
     // constructor
     LinkedList();
 
+    // methods
     void createNode(string id, string name, string grade);
+    void insert_start(string id, string name, string grade);
+    void insert_pos(int pos, string id, string name, string grade);
+    void delete_first();
+    void delete_last();
+    void delete_pos(int pos);
+    void display();
 };
 
 LinkedList::LinkedList() {
@@ -33,7 +41,7 @@ LinkedList::LinkedList() {
     tail = nullptr;
 }
 
-void LinkedList::createNode(const string id, const string name, const string grade) {
+void LinkedList::createNode(string id, string name, string grade) {
     auto *temp = new node;
     temp->courseID = id;
     temp->courseName = name;
@@ -46,6 +54,74 @@ void LinkedList::createNode(const string id, const string name, const string gra
     } else {
         tail->next = temp;
         tail = temp;
+    }
+}
+
+void LinkedList::insert_start(string id, string name, string grade) {
+    auto *temp = new node;
+    temp->courseID = id;
+    temp->courseName = name;
+    temp->grade = grade;
+    temp->next = head;
+    head = temp;
+}
+
+void LinkedList::insert_pos(int pos, string id, string name, string grade) {
+    auto *prev = new node;
+    auto *curr = new node;
+    auto *temp = new node;
+
+    curr = head;
+    for (int i = 1; i < pos; i++) {
+        prev = curr;
+        curr = curr->next;
+    }
+    temp->courseID = id;
+    temp->courseName = name;
+    temp->grade = grade;
+    prev->next = temp;
+    temp->next = curr;
+}
+
+void LinkedList::delete_first() {
+    auto *temp = new node;
+    temp = head;
+    head = head->next;
+    delete temp;
+}
+
+void LinkedList::delete_last() {
+    auto *curr = new node;
+    auto *prev = new node;
+
+    curr = head;
+    while(curr->next != nullptr) {
+        prev = curr;
+        curr = curr->next;
+    }
+    tail = prev;
+    prev->next = nullptr;
+    delete curr;
+}
+
+void LinkedList::delete_pos(int pos) {
+    auto *curr = new node;
+    auto *prev = new node;
+
+    curr = head;
+    for (int i = 1; i < pos; i++) {
+        prev = curr;
+        curr = curr->next;
+    }
+    prev->next = curr->next;
+}
+
+void LinkedList::display() {
+    auto *temp = new node;
+    temp = head;
+    while(temp != nullptr) {
+        cout << temp->courseID << " | " << temp->courseName << " | " << temp->grade << endl;
+        temp = temp->next;
     }
 }
 
